fc2d3734b7b3d645017faf2ab7442f0f23429d2c
LE2HE <leehe4352@gamil.com>
2022-11-04 06:50:01 +0900

    Test #9 : 뷰 테스트 구현 - Event
    이벤트 페이지 테스트 구현

---
+++ src/test/java/com/example/getinline/controller/EventControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.getinline.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(EventController.class)
+class EventControllerTest {
+
+    private final MockMvc mvc;
+
+    public EventControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 이벤트 리스트 페이지")
+    @Test
+    void givenNothing_whenRequestingEventsPage_thenReturnsEventsPage() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/events"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("event/index"))
+                .andDo(print());
+    }
+
+    @DisplayName("[view][GET] 이벤트 세부 정보 페이지")
+    @Test
+    void givenNothing_whenRequestingEventDetailPage_thenReturnsEventDetailPage() throws Exception {
+        // given
+        long eventId = 1L;
+
+        // when & then
+        mvc.perform(get("/events/" + eventId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("event/detail"))
+                .andDo(print());
+    }
+
+}
