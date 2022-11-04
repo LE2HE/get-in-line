581f7f80d1b8a0cc07b67f7552b799bb6aca2bbd
LE2HE <leehe4352@gamil.com>
2022-11-04 20:01:01 +0900

    Feat #11 : APIEventController 기능 재구현
    EventService 사용해 이벤트 조회/생성/수정/삭제 구현

--- src/main/java/com/example/getinline/controller/api/APIEventController.java
+++ src/main/java/com/example/getinline/controller/api/APIEventController.java
@@ -3,65 +3,69 @@ package com.example.getinline.controller.api;
 import com.example.getinline.constant.EventStatus;
 import com.example.getinline.dto.APIDataResponse;
 import com.example.getinline.dto.EventDTO;
+import com.example.getinline.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
 public class APIEventController {
 
+    private final EventService eventService;
+
     @GetMapping("/events")
-    public APIDataResponse<List<EventDTO>> getEvents() {
-        return APIDataResponse.of(List.of(EventDTO.of(
-                1L,
-                "오후 운동",
-                EventStatus.OPENED,
-                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
-                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
-                0,
-                24,
-                "마스크 꼭 착용하세요"
-        )));
+    public APIDataResponse<List<EventDTO>> getEvents(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventStartDatetime,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventEndDatetime
+    ) {
+        List<EventDTO> response = eventService.getEvents(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime
+        ).stream().map(EventDTO::from).toList();
+
+        return APIDataResponse.of(response);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/events")
-    public APIDataResponse<Void> createEvent(@RequestBody EventDTO eventDTO) {
-        return APIDataResponse.empty();
+    public APIDataResponse<String> createEvent(@RequestBody EventDTO eventDTO) {
+        boolean result = eventService.createEvent(eventDTO);
+        return APIDataResponse.of(Boolean.toString(result));
     }
 
     @GetMapping("/events/{eventId}")
     public APIDataResponse<EventDTO> getEvent(@PathVariable Long eventId) {
-        if (eventId.equals(2L)) {
-            return APIDataResponse.empty();
-        }
+        EventDTO eventResponse = EventDTO.from(eventService.getEvent(eventId).orElse(null));
 
-        return APIDataResponse.of(EventDTO.of(
-                1L,
-                "오후 운동",
-                EventStatus.OPENED,
-                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
-                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
-                0,
-                24,
-                "마스크 꼭 착용하세요"
-        ));
+        return APIDataResponse.of(eventResponse);
     }
 
     @PutMapping("/events/{eventId}")
-    public APIDataResponse<Void> modifyEvent(
+    public APIDataResponse<String> modifyEvent(
             @PathVariable Long eventId,
             @RequestBody EventDTO eventDTO
     ) {
-        return APIDataResponse.empty();
+        boolean result = eventService.modifyEvent(eventId, eventDTO);
+        return APIDataResponse.of(Boolean.toString(result));
     }
 
     @DeleteMapping("/events/{eventId}")
-    public APIDataResponse<Void> removeEvent(@PathVariable Long eventId) {
-        return APIDataResponse.empty();
+    public APIDataResponse<String> removeEvent(@PathVariable Long eventId) {
+        boolean result = eventService.removeEvent(eventId);
+
+        return APIDataResponse.of(Boolean.toString(result));
     }
 
 }
